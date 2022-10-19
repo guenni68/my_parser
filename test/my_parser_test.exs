a057745ff5befdc33bc77c10447978fecd6b7f7f
Guenther Schmidt <schmig68@gmail.com>
2022-10-19 09:50:21 +0100

    test setup

--- test/my_parser_test.exs
+++ test/my_parser_test.exs
@@ -1,3 +1,12 @@
 defmodule MyParserTest do
   use MyParserTestSupport
+
+  test "rule1" do
+    parser = from_rule_name("rule1")
+
+    assert {:done, {:ok, _result, ""}} = parser.("1234567890")
+
+    assert {:continue, cont1} = parser.("12345")
+    assert {:done, {:ok, _result, ""}} = cont1.("67890")
+  end
 end
